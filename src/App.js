7723246586f30d8d3ac64b4f1469191ad29a2cae
andrew28051985 <korotkov915@gmail.com>
2020-08-14 22:46:22 +0300

    feat: add section matematic

--- src/App.js
+++ src/App.js
@@ -19,13 +19,16 @@ class App extends Component {
     transactions: [],
     description: '',
     amount: '',
+    resultIncome: 0,
+    resultExpress: 0,
+    totalBalance: 0,
   }
 
   addTransaction = add => {
 
     const transactions = [...this.state.transactions,
         {
-            id: `cmr${(+new Date).toString(16)}`,
+            id: `cmr${(+new Date()).toString(16)}`,
             description: this.state.description,
             amount: this.state.amount,  
             add
@@ -35,16 +38,41 @@ class App extends Component {
     this.setState({ 
         transactions,
         description: '',
-        amount: '',
-    });
+        amount: '',        
+    }, this.getTotalBalance);
   }
 
   addAmount = e =>  {
-    this.setState({amount: e.target.value}); 
+    this.setState({amount: parseFloat(e.target.value)}); 
   }
 
   addDescription = e =>  {
     this.setState({description: e.target.value}); 
+  }
+
+  getIncome() {
+    return this.state.transactions
+    .filter(item => item.add)
+    .reduce((acc, item) => item.amount + acc, 0)
+  }
+
+  getExpress() {
+    return this.state.transactions
+    .filter(item => !item.add)
+    .reduce((acc, item) => item.amount + acc, 0)
+  }
+
+  getTotalBalance() {
+      const resultIncome = this.getIncome();
+      const resultExpress = this.getExpress();
+
+      const totalBalance = resultIncome - resultExpress;
+
+      this.setState({
+        resultIncome,
+        resultExpress,
+        totalBalance,
+      });
   }
     
   render() {
@@ -58,7 +86,11 @@ class App extends Component {
           </header>
           <main>
             <div className="container">
-                <Total/>
+                <Total 
+                    resultExpress={this.state.resultExpress}
+                    resultIncome={this.state.resultIncome}
+                    totalBalance={this.state.totalBalance}
+                />
                 <History 
                     transactions={this.state.transactions}
                 />
